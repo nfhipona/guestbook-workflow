57dd20e8200547bff87841b9ae6d855a469b08e4
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-07-16 21:51:34 +0800

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -8908,7 +8908,7 @@ var require_issue = __commonJS({
       toEntryString(identifier, delimiter, template, max_character_count = 0) {
         const charCount = max_character_count === 0 ? this.bodyText.length : Math.min(max_character_count, this.bodyText.length);
         const contents = this.bodyText.slice(0, charCount);
-        return template.replace("$username", this.author).replace("$profile", this.profile).replace("$date", this.createdAt).replace("$title", this.getTitle(identifier, delimiter)).replace("$content", contents);
+        return template.replaceAll("$username", this.author).replaceAll("$profile", this.profile).replaceAll("$date", this.createdAt).replaceAll("$title", this.getTitle(identifier, delimiter)).replaceAll("$content", contents);
       }
       avatarString() {
         return `<a href="${this.avatarUrl}"><img src="${this.profile}" height="30"/></a>`;
@@ -8919,7 +8919,7 @@ var require_issue = __commonJS({
       getTitle(identifier, delimiter) {
         const titleExp = new RegExp(`^([${identifier}]+.+[${delimiter}])`, "g");
         const trimExp = new RegExp(`^s+|s+$`, "g");
-        const titleContent = this.title.replace(titleExp);
+        const titleContent = this.title.replace(titleExp, "");
         return titleContent.replace(trimExp, "");
       }
     };
@@ -9011,7 +9011,7 @@ async function updateReadme(content) {
 }
 function constructGuestbook(issues = []) {
   if (issues.length === 0) {
-    return EMPTY_TEMPLATE.replace("$username", owner).replace("$repo", repo).replace("$identifier", ENTRY_IDENTIFIER);
+    return EMPTY_TEMPLATE.replaceAll("$username", owner).replaceAll("$repo", repo).replaceAll("$identifier", ENTRY_IDENTIFIER);
   }
   const guestbookAvatars = issues.map((item) => item.avatarString()).join(" ");
   const guestbookComments = issues.map((item) => item.toEntryString(
@@ -9020,7 +9020,7 @@ function constructGuestbook(issues = []) {
     COMMENT_TEMPLATE,
     Number(MAX_CHARACTER_COUNT) || 0
   )).join(" ");
-  const newEntryLink = COMMENT_LINK_TEMPLATE.replace("$username", owner).replace("$repo", repo).replace("$identifier", ENTRY_IDENTIFIER);
+  const newEntryLink = COMMENT_LINK_TEMPLATE.replaceAll("$username", owner).replaceAll("$repo", repo).replaceAll("$identifier", ENTRY_IDENTIFIER);
   return `
         ${guestbookAvatars}
 
