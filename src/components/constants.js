94ef5be96144d0f7b6653b207f230dbdb7b19025
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-08-02 12:11:40 +0800

    Cleanup yml inputs

--- src/components/constants.js
+++ src/components/constants.js
@@ -4,10 +4,6 @@ const github = require('@actions/github');
 const GITHUB_TOKEN = core.getInput('gh_token');
 const MAX_DISPLAY_COUNT = core.getInput('max_display_count');
 const MAX_CHARACTER_COUNT = core.getInput('max_character_count');
-const COMMITTER_USERNAME = core.getInput('committer_username');
-const COMMITTER_EMAIL = core.getInput('committer_email');
-const COMMITTER_MESSAGE = core.getInput('commit_message');
-const ENABLE_KEEP_ALIVE = core.getInput('enable_keepalive');
 const MAX_RETRY_COUNT = core.getInput('max_retry_count');
 const RETRY_WAIT_TIME = core.getInput('retry_wait_time');
 const ENTRY_IDENTIFIER = core.getInput('entry_identifier');
@@ -28,10 +24,6 @@ module.exports = {
     GITHUB_TOKEN,
     MAX_DISPLAY_COUNT,
     MAX_CHARACTER_COUNT,
-    COMMITTER_USERNAME,
-    COMMITTER_EMAIL,
-    COMMITTER_MESSAGE,
-    ENABLE_KEEP_ALIVE,
     MAX_RETRY_COUNT,
     RETRY_WAIT_TIME,
     ENTRY_IDENTIFIER,
